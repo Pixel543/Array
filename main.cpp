d11365deadfd82e952b8c9788d11d8d32d669dff
Volodimyr Bobrovnyk <vladimirbobrovnyk@gmail.com>
2025-05-12 19:08:09 +0300

    adding num

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//#include "stdafx.h"
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	cout << "Git hello \n";
+
+    int num = 1;
+    std::cout << "branch- " << num << std::endl;
+
+    return 0;
+}
